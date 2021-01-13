e03f7c1fb653f6393a7dd1d080ce926fe1dc5b70
kevinam99 <kevinam99@gmail.com>
2021-01-13 21:35:00 +0530

    Using Webhook method independently

--- setWebhook.js
+++ setWebhook.js
@@ -12,36 +12,35 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
 
 const endpoint = `/messengerEndpoint`
 
-const setWebhook = () => {
-    
-    // Adds support for GET requests to our webhook
-    app.get(endpoint, (req, res) => {
-    
-        // Your verify token. Should be a random string.
-        let VERIFY_TOKEN = PAGE_ACCESS_TOKEN
-          
-        // Parse the query params
-        let mode = req.query['hub.mode'];
-        let token = req.query['hub.verify_token'];
-        let challenge = req.query['hub.challenge'];
-          
-        // Checks if a token and mode is in the query string of the request
-        if (mode && token) {
-        
-          // Checks the mode and token sent is correct
-          if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-            
-            // Responds with the challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-          
-          } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);      
-          }
-        }
-      });
-} 
 
-module.exports = setWebhook
+// Adds support for GET requests to our webhook
+app.get(endpoint, (req, res) => {
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = PAGE_ACCESS_TOKEN
+        
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+        
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+    
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.status(200).send(challenge);
+        
+        } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+        }
+    }
+    });
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+
 
